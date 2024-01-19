2c15e11e915e73bc480d761d77d1fb684a5e8b6e
Adarshakarki <111886610+Adarshakarki@users.noreply.github.com>
2024-01-19 13:13:00 +0545

    Add files via upload

--- login.js
+++ login.js
@@ -1,43 +1,26 @@
-function toggleForms() {
-    var loginForm = document.querySelector('form[action="login.php"]');
-    var signupForm = document.getElementById('signupForm');
-
-    // Toggle the display property to switch between login and signup forms
-    if (loginForm.style.display === 'none' || loginForm.style.display === '') {
-        loginForm.style.display = 'block';
-        signupForm.style.display = 'none';
-    } else {
-        loginForm.style.display = 'none';
-        signupForm.style.display = 'block';
-    }
-}
-
 function togglePassword() {
     var passwordInput = document.getElementById("password");
-    var eyeIcon = document.querySelector('.eye-icon');
 
     if (passwordInput.type === "password") {
         passwordInput.type = "text";
-        eyeIcon.classList.remove('bx-hide');
-        eyeIcon.classList.add('bx-show');
     } else {
         passwordInput.type = "password";
-        eyeIcon.classList.remove('bx-show');
-        eyeIcon.classList.add('bx-hide');
     }
 }
 
-function toggleSignupPassword() {
-    var signupPasswordInput = document.getElementById("signupPassword");
-    var signupEyeIcon = document.querySelector('.signup-eye-icon');
+function submitForm(action) {
+    var form = document.getElementById('loginForm');
+    var formData = new FormData(form);
+    formData.append('action', action);
+    fetch(form.action, {
+            method: form.method,
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => handleResponse(data))
+        .catch(error => console.error('Error:', error));
+}
 
-    if (signupPasswordInput.type === "password") {
-        signupPasswordInput.type = "text";
-        signupEyeIcon.classList.remove('bx-hide');
-        signupEyeIcon.classList.add('bx-show');
-    } else {
-        signupPasswordInput.type = "password";
-        signupEyeIcon.classList.remove('bx-show');
-        signupEyeIcon.classList.add('bx-hide');
-    }
+function setSignupAction() {
+    submitForm('signup');
 }
